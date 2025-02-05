b2ecc6dfc1d5d077e5a5aaa47c79b0702fc9ac51
tulsyanshubham <tulsyanshubham3639@gmail.com>
2025-02-05 16:19:51 +0530

    flask done

--- for_node_mongo/index.js
+++ for_node_mongo/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-const database = "mongodb://localhost:27017/testDocker";
+const database = "mongodb://mymongo:27017/testDockery";
+//                             ^--- name of mongo container
 
 mongoose.connect(database)
     .then(() => {
@@ -9,3 +12,13 @@ mongoose.connect(database)
     .catch(() => {
         console.log('Connection to DB failed');
     });
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:"hello world!"
+    })
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
